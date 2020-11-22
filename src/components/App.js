86c1994b6c3dd927228f6cef00de56709f380783
Robbie <rob.w.sherman@gmail.com>
2020-11-22 13:28:11 +0000

    Add redaxios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'redaxios';
+
 
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID PytwXLTtCi4_Gro57KB3FueJQQuVYOcsJdMmpN9pNj4'
+      }
+    })
   }
 
   render() {
